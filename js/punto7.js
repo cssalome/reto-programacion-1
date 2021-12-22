52330b363830ffadeac58d8784d71da919050924
Carla Serrano <carlacarly8@gmail.com>
2021-12-22 09:27:58 -0300

    terminado

--- js/punto7.js
+++ js/punto7.js
@@ -1,9 +1,8 @@
 const nombre = prompt("Escribe tu nombre", "");
-const edad = prompt("Escribe tu edad", "");
-const fechaActual = new Date();
-
-const edadProxima = edad + 1;
+const edad = Number(prompt("Escribe tu edad", ""));
 
 alert(
-  `Hola ${nombre} el año pasado tenías ${edad} años y el próximo año cumplirás ${edadProxima} años`
+  `Hola ${nombre} el año pasado tenías ${
+    edad - 1
+  } años y el próximo año cumplirás ${edad + 1} años`
 );
